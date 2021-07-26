06bc37e89eba2fb64b4ea9d49dd7d116e086f946
segeok89 <lgdelrosario@outlook.com>
2021-07-26 23:06:55 +0800

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,83 @@
+// Set a variable for our button element.
+const scrollToTopButton = document.getElementById('js-top')
+
+// Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
+const scrollFunc = () => {
+  // Get the current scroll value
+  let y = window.scrollY
+
+  // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
+  if (y > 120) {
+    scrollToTopButton.className = 'top-link show'
+  } else {
+    scrollToTopButton.className = 'top-link hide'
+  }
+}
+
+window.addEventListener('scroll', scrollFunc)
+
+const scrollToTop = () => {
+  // Let's set a variable for the number of pixels we are from the top of the document.
+  const c = document.documentElement.scrollTop || document.body.scrollTop
+
+  if (c > 0) {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+}
+
+// When the button is clicked, run our ScrolltoTop function above!
+scrollToTopButton.onclick = function (e) {
+  e.preventDefault()
+  scrollToTop()
+}
+
+// counter
+
+const counters = document.querySelectorAll('.counting')
+const speed = 200
+
+counters.forEach((counter) => {
+  const animate = () => {
+    const value = +counter.getAttribute('data-count')
+    const data = +counter.innerText
+
+    const time = value / speed
+    if (data < value) {
+      counter.innerText = Math.ceil(data + time)
+      setTimeout(animate, 1)
+    } else {
+      counter.innerText = value
+    }
+  }
+
+  animate()
+})
+
+// isotope
+// external js: isotope.pkgd.js
+var grid = document.querySelector('.grid')
+
+var iso = new Isotope(grid, {
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  masonry: {
+    columnWidth: '.grid-sizer',
+  },
+})
+
+grid.addEventListener('click', function (event) {
+  var gridItem = document.querySelectorAll('.grid-item')
+  ;[].forEach.call(gridItem, function (el) {
+    el.classList.remove('is-expanded')
+  })
+
+  var target = event.target
+  // only click on itemContent
+  if (!target.classList.contains('grid-item-content')) {
+    return
+  }
+
+  var itemElem = target.parentNode
+  itemElem.classList.toggle('is-expanded')
+  iso.layout()
+})
---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express')
+var path = require('path')
+var app = express()
+
+// set the view engine to ejs
+app.set('view engine', 'ejs')
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function (req, res) {
+  res.render('pages/index')
+})
+
+// about page
+app.get('/about', function (req, res) {
+  res.render('pages/about')
+})
+
+// gallery page
+app.get('/gallery', function (req, res) {
+  res.render('pages/gallery')
+})
+
+// menu page
+app.get('/menu', function (req, res) {
+  res.render('pages/menu')
+})
+
+// contact page
+app.get('/contact', function (req, res) {
+  res.render('pages/contact')
+})
+
+// static files
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+app.listen(8080)
+console.log('Server is listening on port 8080')
