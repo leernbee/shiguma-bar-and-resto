0428dd683a4224d8c4c1397d0ef37e1c0b74e364
segeok89 <lgdelrosario@outlook.com>
2021-07-27 12:02:04 +0800

    console error

--- public/js/main.js
+++ public/js/main.js
@@ -69,32 +69,3 @@ let observer = new IntersectionObserver(inViewCallback, options) // create a new
 observedElements.forEach((element) => {
   observer.observe(element) // run the observer
 })
-
-// isotope
-// external js: isotope.pkgd.js
-var grid = document.querySelector('.grid')
-
-var iso = new Isotope(grid, {
-  itemSelector: '.grid-item',
-  percentPosition: true,
-  masonry: {
-    columnWidth: '.grid-sizer',
-  },
-})
-
-grid.addEventListener('click', function (event) {
-  var gridItem = document.querySelectorAll('.grid-item')
-  ;[].forEach.call(gridItem, function (el) {
-    el.classList.remove('is-expanded')
-  })
-
-  var target = event.target
-  // only click on itemContent
-  if (!target.classList.contains('grid-item-content')) {
-    return
-  }
-
-  var itemElem = target.parentNode
-  itemElem.classList.toggle('is-expanded')
-  iso.layout()
-})
