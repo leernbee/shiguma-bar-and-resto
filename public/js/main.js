316d41b952f4f59bc66c14d6f65210a14d483dd4
segeok89 <lgdelrosario@outlook.com>
2021-08-06 21:05:22 +0800

    update

---
+++ public/js/main.js
@@ -0,0 +1,71 @@
+// Set a variable for our button element.
+const scrollToTopButton = document.getElementById('js-top')
+
+// Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
+const scrollFunc = () => {
+  // Get the current scroll value
+  let y = window.scrollY
+
+  // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
+  if (y > 120) {
+    scrollToTopButton.className = 'top-link show'
+  } else {
+    scrollToTopButton.className = 'top-link hide'
+  }
+}
+
+window.addEventListener('scroll', scrollFunc)
+
+const scrollToTop = () => {
+  // Let's set a variable for the number of pixels we are from the top of the document.
+  const c = document.documentElement.scrollTop || document.body.scrollTop
+
+  if (c > 0) {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+}
+
+// When the button is clicked, run our ScrolltoTop function above!
+scrollToTopButton.onclick = function (e) {
+  e.preventDefault()
+  scrollToTop()
+}
+
+// observer
+let observedElements = document.querySelectorAll('.inview-element') // Define the elements you want to intiate an action on
+
+const options = {
+  //define your options
+  threshold: 0.5,
+}
+
+const inViewCallback = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // const counters = document.querySelectorAll('.counting')
+      // const speed = 200
+      // counters.forEach((counter) => {
+      //   const animate = () => {
+      //     const value = +counter.getAttribute('data-count')
+      //     const data = +counter.innerText
+      //     const time = value / speed
+      //     if (data < value) {
+      //       counter.innerText = Math.ceil(data + time)
+      //       setTimeout(animate, 1)
+      //     } else {
+      //       counter.innerText = value
+      //     }
+      //   }
+      //   animate()
+      // })
+    } else {
+      // OPTIONAL, in case you want to do something once the intersection is done
+    }
+  })
+}
+
+let observer = new IntersectionObserver(inViewCallback, options) // create a new instance using our callback which contains our elements and actions, using the options we defined
+
+observedElements.forEach((element) => {
+  observer.observe(element) // run the observer
+})
---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express')
+var path = require('path')
+var app = express()
+
+// set the view engine to ejs
+app.set('view engine', 'ejs')
+
+// static files
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+// index page
+app.get('/', function (req, res) {
+  res.render('pages/index')
+})
+
+// about page
+app.get('/about', function (req, res) {
+  res.render('pages/about')
+})
+
+// gallery page
+app.get('/gallery', function (req, res) {
+  res.render('pages/gallery')
+})
+
+// menu page
+app.get('/menu', function (req, res) {
+  res.render('pages/menu')
+})
+
+// contact page
+app.get('/contact', function (req, res) {
+  res.render('pages/contact')
+})
+
+app.listen(3000)
+console.log('Server is listening on port 3000')
